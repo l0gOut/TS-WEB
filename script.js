da5be556d2c53e350e9070e7bfb5109309716beb
Korka <daniil.petrunin2013@yandex.ru>
2023-03-29 05:33:08 +0300

    add comments

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-export {};
+import CommentBlog from "./commentBlog.js";
+const comment = document.querySelector(".comment-block");
+const successComment = document.querySelector(".success-comment");
+const commentContainer = document.querySelector(".comment-container");
+successComment === null || successComment === void 0 ? void 0 : successComment.addEventListener("click", createComment);
+comment === null || comment === void 0 ? void 0 : comment.addEventListener("input", e => {
+    const commentText = e.target;
+    if (commentText.value.trim() && successComment) {
+        successComment.classList.add("active");
+    }
+    else if (successComment) {
+        successComment.classList.remove("active");
+    }
+});
+function createComment() {
+    if (comment === null || comment === void 0 ? void 0 : comment.value.trim()) {
+        const CommentBlock = new CommentBlog(comment.value.trim());
+        const newComment = document.createElement("div");
+        newComment.innerHTML = CommentBlock.toString();
+        commentContainer === null || commentContainer === void 0 ? void 0 : commentContainer.appendChild(newComment);
+        comment.value = "";
+    }
+}
--- script.ts
+++ script.ts
@@ -1,1 +1,32 @@
 import CommentBlog from "./commentBlog.js";
+
+const comment: HTMLInputElement | null =
+  document.querySelector(".comment-block");
+const successComment: HTMLElement | null =
+  document.querySelector(".success-comment");
+const commentContainer: HTMLElement | null =
+  document.querySelector(".comment-container");
+
+successComment?.addEventListener("click", createComment);
+
+comment?.addEventListener("input", e => {
+  const commentText: HTMLInputElement = <HTMLInputElement>e.target;
+
+  if (commentText.value.trim() && successComment) {
+    successComment.classList.add("active");
+  } else if (successComment) {
+    successComment.classList.remove("active");
+  }
+});
+
+function createComment(): void {
+  if (comment?.value.trim()) {
+    const CommentBlock: CommentBlog = new CommentBlog(comment.value.trim());
+    const newComment: HTMLElement = document.createElement("div");
+
+    newComment.innerHTML = CommentBlock.toString();
+
+    commentContainer?.appendChild(newComment);
+    comment.value = "";
+  }
+}
